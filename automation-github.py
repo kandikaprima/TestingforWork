c783259483fd27ce12a32ab9abc3e63d06de50b7
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2024-09-24 14:13:03 +0800

    Add azka Project by kandika

--- automation-github.py
+++ automation-github.py
@@ -1,26 +1,20 @@
 import git
 
-def git_pull_push(repo_path, file = ".", msg = "Add new student Project"):
-  """
-  Melakukan git pull dan git push ke repositori yang ditentukan.
+studentName = "azka"
+teacherName = "kandika"
 
-  Args:
-    repo_path: Path ke direktori repositori Git.
-    branch: Nama branch yang akan di-pull dan di-push.
-    remote: Nama remote yang akan digunakan.
-  """
-
+def git_pull_push(repo_path, student, teacher):
   try:
     repo = git.Repo(repo_path)
 
     repo.git.execute("git pull")
-    repo.git.execute(f"git add {file}")
-    repo.git.execute(f'''git commit -m "{msg}"''')
+    repo.git.execute("git add .")
+    repo.git.execute(f'''git commit -m "Add {student} Project by {teacher}"''')
     repo.git.execute("git push")
 
-    print(f"Git pull dan push berhasil untuk {repo_path}")
+    print(f"File project {student} pada folder {repo_path} berhasil di upload")
   except Exception as e:
     print(f"Terjadi kesalahan: {str(e)}")
 
 repo_path = "D:/Koding Next/06 - Work Project/github/TestingforWork"
-git_pull_push(repo_path)
+git_pull_push(repo_path, studentName, teacherName)
